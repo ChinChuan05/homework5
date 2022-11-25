58a48a7dd63c8515cd9995420b53565f4be2f834
Brian <0103.brian@gmail.com>
2022-11-25 14:57:53 +0800

    finish to p05

---
+++ p06/HMW/p05.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main() {
+	int a;
+	int *aPtr;
+
+	a = 7;
+	aPtr = &a;
+
+	printf("The address of a is %p"
+		"\nThe value of aPter is %p\n\n", &a, aPtr);
+
+	printf("The value of a is %d"
+		"\nThe value of *aPtr is %d", a, *aPtr);
+
+	printf("\n\nShowing that * and & are complements of "
+		"each other\n&*aPtr = %p"
+		"\n*&aPtr = %p\n", &*aPtr, *&aPtr);
+
+	system("pause");
+	return 0;
+}
